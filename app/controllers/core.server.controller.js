cb50ddac17ef633e3249feb8998fc11145d7873a
Jaikumar Bhambhwani <jai.bhambhwani@gmail.com>
2015-08-16 17:29:23 -0700

    Add deeplink lib

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -8,4 +8,8 @@ exports.index = function(req, res) {
 		user: req.user || null,
 		request: req
 	});
-};
+
+exports.deeplink = deeplink({
+    fallback: 'http://google.com',
+    ios_store_link: 'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4',
+});
--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core.server.controller');
 	app.route('/').get(core.index);
+
+	// Deeplinking into apps
+	app.route('/deeplink*').get(core.deeplink);
+
 };
